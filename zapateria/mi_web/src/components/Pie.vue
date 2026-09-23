1b4d9330a8f47b46343310d6a90f4285197daa41
Mars <231862100+mariadelosangeles96-pixel@users.noreply.github.com>
2026-09-23 10:27:10 +0200

    zapateria. Componentes creados

---
+++ zapateria/mi_web/src/components/Pie.vue
@@ -0,0 +1,12 @@
+<!-- Pie.vue -->
+<template>
+    <footer id="contacto">STEPUP - Tu siguiente paso empieza aqui.</footer>
+</template>
+<style scoped>
+footer {
+    background: #172033;
+    color: white;
+    padding: 28px;
+    text-align: center;
+}
+</style>
