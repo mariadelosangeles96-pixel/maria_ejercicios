b5d4a2033e394d529fdeb460c6ab91b8c0abb510
Mars <231862100+mariadelosangeles96-pixel@users.noreply.github.com>
2026-09-25 14:20:38 +0200

    actividades. push final de clase

--- zapateria/mi_web/src/App.vue
+++ zapateria/mi_web/src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
+import { ref } from 'vue'
 import Cabecera from './components/Cabecera.vue'
 import Inicio from './components/Inicio.vue'
 import Modelos from './components/Modelos.vue'
 import Pie from './components/Pie.vue'
+import Catalogo from './components/catalogo.vue'
+
+const mostrarCatalogo = ref(false)
 </script>
 
 <template>
-  <Cabecera />
+  <Cabecera @abrir-modelos="mostrarCatalogo = true" />
   <main>
-    <Inicio />
+    <Inicio @abrir-modelos="mostrarCatalogo = true"/>
     <Modelos />
   </main>
   <Pie />
+
+  <Catalogo :visible="mostrarCatalogo" @cerrar="mostrarCatalogo = false" />
 </template>
--- zapateria/mi_web/src/components/Inicio.vue
+++ zapateria/mi_web/src/components/Inicio.vue
@@ -3,6 +3,7 @@
     import Detalle from './detalle.vue'
 
     const mostrarDetalle = ref(false)
+    defineEmits(['abrir-modelos'])
 </script>
 
 <template>
@@ -11,7 +12,7 @@
             <p>NUEVA COLECCION</p>
             <h1>Corre a tu manera.</h1>
             <p>Zapatillas ligeras, comodas y preparadas para tu ritmo.</p>
-            <a href="#modelos">Ver modelos</a>
+            <a href="#modelos" @click.prevent="$emit('abrir-modelos')">Ver modelos</a>
         </div>
         <img src="https://images.unsplash.com/photo-1552346154-21d32810aba3?auto=format&fit=crop&w=900&q=80"
             alt="Zapatillas deportivas blancas"
